eaecf1f3965c0be94f09fe6a8cf0390c6822bdd4
mashruf99 <bemashrufislam@gmail.com>
2022-05-09 22:40:59 +0600

    easy

--- Weighted Averages.c
+++ Weighted Averages.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
 int main() {
- float x,y,z,m;
- int i;
-for( i=0 ; i<3 ; i++){
-    scanf("%f %f %f",&x, &y ,&z);
-    m=(x+y+z)/3;
-    printf("%.1f\n",m);
- }
+ float x,y,z,sum=0;
+ int i,j,m;
+  scanf("%d",&m);
+for( i=0 ; i<m ; i++){
+        for(j=0 ; j<3 ;j++ ){
+    scanf("%f %f %f", &x, &y, &z);
+     x *=2;
+     y *=3;
+     z *=5;
+     for (j=0 ; j<3 ; j++){
+        sum=x+y+z;
+     }
+        }
+    printf("%.1f\n",sum/10);
+}
 
 
     return 0;
